7e33896c02c179f5a6381ebe40d3fbfd076e6973
niccellular <79813408+niccellular@users.noreply.github.com>
2024-02-07 15:36:09 -0500

    1.0.5 - New TAKPacket protobuf
    Use new TAKPacket protobuf for optimized wire size over Meshtastic.

--- app/src/main/java/com/atakmap/android/meshtastic/MeshtasticMapComponent.java
+++ app/src/main/java/com/atakmap/android/meshtastic/MeshtasticMapComponent.java
@@ -11,23 +11,29 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.widget.Toast;
 
-import com.atakmap.android.maps.AbstractMapComponent;
-import com.geeksville.mesh.MessageStatus;
-
-
 import com.atakmap.android.maps.MapView;
-import com.atakmap.comms.CommsMapComponent;
+import com.atakmap.android.maps.AbstractMapComponent;
+import com.atakmap.android.meshtastic.plugin.R;
+import com.atakmap.coremap.log.Log;
 import com.atakmap.coremap.cot.event.CotEvent;
+import com.atakmap.coremap.cot.event.CotDetail;
+import com.atakmap.comms.CommsMapComponent;
+
+import com.geeksville.mesh.MessageStatus;
+import com.geeksville.mesh.ATAKProtos;
+import com.geeksville.mesh.Portnums;
 import com.geeksville.mesh.DataPacket;
 import com.geeksville.mesh.IMeshService;
 
-import com.atakmap.coremap.log.Log;
-import com.atakmap.android.meshtastic.plugin.R;
 import com.paulmandal.atak.libcotshrink.pub.api.CotShrinker;
 import com.paulmandal.atak.libcotshrink.pub.api.CotShrinkerFactory;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -67,6 +73,7 @@ public class MeshtasticMapComponent extends AbstractMapComponent implements Comm
     public static final String ACTION_MESH_CONNECTED = "com.geeksville.mesh.MESH_CONNECTED";
     public static final String ACTION_MESH_DISCONNECTED = "com.geeksville.mesh.MESH_DISCONNECTED";
     public static final String ACTION_RECEIVED_ATAK_FORWARDER = "com.geeksville.mesh.RECEIVED.ATAK_FORWARDER";
+    public static final String ACTION_RECEIVED_ATAK_PLUGIN = "com.geeksville.mesh.RECEIVED.ATAK_PLUGIN";
     public static final String ACTION_RECEIVED_NODEINFO_APP = "com.geeksville.mesh.RECEIVED.NODEINFO_APP";
     public static final String ACTION_RECEIVED_POSITION_APP = "com.geeksville.mesh.RECEIVED.POSITION_APP";
     public static final String ACTION_NODE_CHANGE = "com.geeksville.mesh.NODE_CHANGE";
@@ -114,55 +121,162 @@ public class MeshtasticMapComponent extends AbstractMapComponent implements Comm
         if (mConnectionState == ServiceConnectionState.DISCONNECTED)
             return;
 
-        Log.d(TAG, cotEvent.toString());
         DataPacket dp;
 
-        byte[] cotAsBytes = cotShrinker.toByteArrayLossy(cotEvent);
+        Log.d(TAG, cotEvent.toString());
+        CotDetail cotDetail = cotEvent.getDetail();
+        Log.d(TAG, cotDetail.toString());
 
-        Log.d(TAG, "Size: " + cotAsBytes.length);
+        // is this our self marker outgoing CoT, if so do tak_payload portnum
+        if (cotEvent.getUID().equals(getMapView().getSelfMarker().getUID())) {
+            Log.d(TAG, "self marker PLI");
+            
+            int battery = 0, course = 0, speed = 0;
+            String role = null, name = null;
+            String callsign = getMapView().getDeviceCallsign();
+            double alt = getMapView().getSelfMarker().getPoint().getAltitude();
+            double lat = getMapView().getSelfMarker().getPoint().getLatitude();
+            double lng = getMapView().getSelfMarker().getPoint().getLongitude();
 
-        if (cotAsBytes.length < 236) {
-            Log.d(TAG, "Small send");
-            dp = new DataPacket("^all", cotAsBytes, ATAK_FORWARDER, DataPacket.ID_LOCAL, System.currentTimeMillis(), 0, MessageStatus.UNKNOWN, 3, 0);
+            Log.d(TAG, String.format("GPS: %f %f %f", alt, lat ,lng));
+
             try {
-                mMeshService.send(dp);
-            } catch (RemoteException e) {
-                throw new RuntimeException(e);
-            }
-            return;
-        }
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(true);
+                XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput( new StringReader(cotDetail.toString()));
+                int eventType = xpp.getEventType();
 
-        List<byte[]> chunkList = divideArray(cotAsBytes, 200);
-
-        int chunks = (int) Math.floor(cotAsBytes.length / 200);
-        chunks++;
-        Log.d(TAG, "Sending " + chunks);
-
-        byte[] chunk_hdr = String.format(Locale.US,"CHUNK_%d_", cotAsBytes.length).getBytes();
-
-        for (byte[] c : chunkList) {
-            byte[] combined = new byte[chunk_hdr.length + c.length];
-            try {
-                System.arraycopy(chunk_hdr, 0, combined, 0, chunk_hdr.length);
-                System.arraycopy(c, 0, combined, chunk_hdr.length, c.length);
-            } catch (Exception e) {
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if(eventType == XmlPullParser.START_TAG) {
+                        if (xpp.getName().equalsIgnoreCase("contact")) {
+                            int attributeCount = xpp.getAttributeCount();
+                            Log.d(TAG, "Contact has " + + attributeCount);
+                            for (int i=0; i< attributeCount; i++) {
+                                Log.d(TAG, xpp.getAttributeName(i) + "=" + xpp.getAttributeValue(i));
+                                if (xpp.getAttributeName(i).equalsIgnoreCase("callsign"))
+                                    callsign = xpp.getAttributeValue(i);
+                            }
+                        } else if (xpp.getName().equalsIgnoreCase("__group")) {
+                            int attributeCount = xpp.getAttributeCount();
+                            Log.d(TAG, "__group has " + + attributeCount);
+                            for (int i=0; i< attributeCount; i++) {
+                                Log.d(TAG, xpp.getAttributeName(i) + "=" + xpp.getAttributeValue(i));
+                                if (xpp.getAttributeName(i).equalsIgnoreCase("role"))
+                                    role = xpp.getAttributeValue(i);
+                                else if (xpp.getAttributeName(i).equalsIgnoreCase("name"))
+                                    name = xpp.getAttributeValue(i);
+                            }
+                        } else if (xpp.getName().equalsIgnoreCase("status")) {
+                            int attributeCount = xpp.getAttributeCount();
+                            Log.d(TAG, "status has " + + attributeCount);
+                            for (int i=0; i< attributeCount; i++) {
+                                Log.d(TAG, xpp.getAttributeName(i) + "=" + xpp.getAttributeValue(i));
+                                if (xpp.getAttributeName(i).equalsIgnoreCase("battery"))
+                                    battery = Integer.parseInt(xpp.getAttributeValue(i));
+                            }
+                        } else if (xpp.getName().equalsIgnoreCase("track")) {
+                            int attributeCount = xpp.getAttributeCount();
+                            Log.d(TAG, "track has " + + attributeCount);
+                            for (int i=0; i< attributeCount; i++) {
+                                Log.d(TAG, xpp.getAttributeName(i) + "=" + xpp.getAttributeValue(i));
+                                if (xpp.getAttributeName(i).equalsIgnoreCase("course"))
+                                    course = Double.valueOf(xpp.getAttributeValue(i)).intValue();
+                                else if (xpp.getAttributeName(i).equalsIgnoreCase("speed"))
+                                    speed = Double.valueOf(xpp.getAttributeValue(i)).intValue();
+                            }
+                        }
+                    }
+                    eventType = xpp.next();
+                }
+            } catch (XmlPullParserException | IOException e) {
                 e.printStackTrace();
             }
-            // send out 1 chunk
-            dp = new DataPacket("^all", combined, ATAK_FORWARDER, DataPacket.ID_LOCAL, System.currentTimeMillis(), 0, MessageStatus.UNKNOWN, 3, 0);
+
+            // represent 1e-7 as a double
+
+            double divisor = 1e-7;
+
+            ATAKProtos.Contact.Builder contact = ATAKProtos.Contact.newBuilder();
+            contact.setCallsign(callsign);
+
+            ATAKProtos.Group.Builder group = ATAKProtos.Group.newBuilder();
+            group.setRole(ATAKProtos.MemberRole.valueOf(role.replace(" ", "")));
+            group.setTeam(ATAKProtos.Team.valueOf(name.replace(" ", "")));
+
+            ATAKProtos.Status.Builder status = ATAKProtos.Status.newBuilder();
+            status.setBattery(battery);
+
+            ATAKProtos.PLI.Builder pli = ATAKProtos.PLI.newBuilder();
+            pli.setAltitude(Double.valueOf(alt).intValue());
+            pli.setLatitudeI((int) (lat / divisor));
+            pli.setLongitudeI((int) (lng / divisor));
+            pli.setCourse(course);
+            pli.setSpeed(speed);
+
+            ATAKProtos.TAKPacket.Builder tak_packet = ATAKProtos.TAKPacket.newBuilder();
+            tak_packet.setContact(contact);
+            tak_packet.setStatus(status);
+            tak_packet.setGroup(group);
+            tak_packet.setPli(pli);
+
+            Log.d(TAG, "Total wire size for TAKPacket: " + tak_packet.build().toByteArray().length);
+
+            dp = new DataPacket("^all", tak_packet.build().toByteArray(), Portnums.PortNum.ATAK_PLUGIN_VALUE, DataPacket.ID_LOCAL, System.currentTimeMillis(), 0, MessageStatus.UNKNOWN, 3, 0);
             try {
                 mMeshService.send(dp);
             } catch (RemoteException e) {
                 throw new RuntimeException(e);
             }
-        }
 
-        // We're done chunking
-        dp = new DataPacket("^all", new byte[] {'E', 'N', 'D'}, ATAK_FORWARDER, DataPacket.ID_LOCAL, System.currentTimeMillis(), 0, MessageStatus.UNKNOWN, 3, 0);
-        try {
-            mMeshService.send(dp);
-        } catch (RemoteException e) {
-            throw new RuntimeException(e);
+        } else {
+            byte[] cotAsBytes = cotShrinker.toByteArrayLossy(cotEvent);
+
+            Log.d(TAG, "Size: " + cotAsBytes.length);
+
+            if (cotAsBytes.length < 236) {
+                Log.d(TAG, "Small send");
+                dp = new DataPacket("^all", cotAsBytes, ATAK_FORWARDER, DataPacket.ID_LOCAL, System.currentTimeMillis(), 0, MessageStatus.UNKNOWN, 3, 0);
+                try {
+                    mMeshService.send(dp);
+                } catch (RemoteException e) {
+                    throw new RuntimeException(e);
+                }
+                return;
+            }
+
+            List<byte[]> chunkList = divideArray(cotAsBytes, 200);
+
+            int chunks = (int) Math.floor(cotAsBytes.length / 200);
+            chunks++;
+            Log.d(TAG, "Sending " + chunks);
+
+            byte[] chunk_hdr = String.format(Locale.US, "CHUNK_%d_", cotAsBytes.length).getBytes();
+
+            for (byte[] c : chunkList) {
+                byte[] combined = new byte[chunk_hdr.length + c.length];
+                try {
+                    System.arraycopy(chunk_hdr, 0, combined, 0, chunk_hdr.length);
+                    System.arraycopy(c, 0, combined, chunk_hdr.length, c.length);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                // send out 1 chunk
+                dp = new DataPacket("^all", combined, ATAK_FORWARDER, DataPacket.ID_LOCAL, System.currentTimeMillis(), 0, MessageStatus.UNKNOWN, 3, 0);
+                try {
+                    mMeshService.send(dp);
+                } catch (RemoteException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            // We're done chunking
+            dp = new DataPacket("^all", new byte[]{'E', 'N', 'D'}, ATAK_FORWARDER, DataPacket.ID_LOCAL, System.currentTimeMillis(), 0, MessageStatus.UNKNOWN, 3, 0);
+            try {
+                mMeshService.send(dp);
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -178,6 +292,8 @@ public class MeshtasticMapComponent extends AbstractMapComponent implements Comm
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(ACTION_RECEIVED_ATAK_FORWARDER);
         intentFilter.addAction("com.geeksville.mesh.RECEIVED.257");
+        intentFilter.addAction(ACTION_RECEIVED_ATAK_PLUGIN);
+        intentFilter.addAction("com.geeksville.mesh.RECEIVED.72");
         intentFilter.addAction(ACTION_NODE_CHANGE);
         intentFilter.addAction(ACTION_MESH_CONNECTED);
         intentFilter.addAction(ACTION_MESH_DISCONNECTED);
--- app/src/main/java/com/atakmap/android/meshtastic/MeshtasticReceiver.java
+++ app/src/main/java/com/atakmap/android/meshtastic/MeshtasticReceiver.java
@@ -3,15 +3,25 @@ package com.atakmap.android.meshtastic;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Parcel;
 import android.os.RemoteException;
+import android.util.Xml;
 
 import com.atakmap.android.cot.CotMapComponent;
+import com.atakmap.coremap.cot.event.CotDetail;
 import com.atakmap.coremap.cot.event.CotEvent;
+import com.atakmap.coremap.cot.event.CotPoint;
 import com.atakmap.coremap.log.Log;
 import com.atakmap.coremap.maps.time.CoordinatedTime;
+
+import com.geeksville.mesh.ATAKProtos;
 import com.geeksville.mesh.DataPacket;
+
 import com.geeksville.mesh.MessageStatus;
 import com.geeksville.mesh.NodeInfo;
+import com.geeksville.mesh.Portnums;
+import com.google.protobuf.InvalidProtocolBufferException;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,12 +54,26 @@ public class MeshtasticReceiver extends BroadcastReceiver {
                 MeshtasticMapComponent.mw.setIcon("red");
             }
         }
+        else if (action.equals(MeshtasticMapComponent.ACTION_MESH_DISCONNECTED)) {
+            String extraConnected = intent.getStringExtra(MeshtasticMapComponent.EXTRA_DISCONNECTED);
+            boolean connected = extraConnected.equals(MeshtasticMapComponent.STATE_DISCONNECTED);
+            Log.d(TAG, "Received ACTION_MESH_DISCONNECTED: " + extraConnected);
+            if (connected) {
+                MeshtasticMapComponent.mConnectionState = MeshtasticMapComponent.ServiceConnectionState.DISCONNECTED;
+                MeshtasticMapComponent.mw.setIcon("red");
+            }
+        }
         else if (action.equals(MeshtasticMapComponent.ACTION_MESSAGE_STATUS)) {
             int id = intent.getIntExtra(MeshtasticMapComponent.EXTRA_PACKET_ID, 0);
             MessageStatus status = intent.getParcelableExtra(MeshtasticMapComponent.EXTRA_STATUS);
             Log.d(TAG, "Message Status ID: " + id + " Status: " + status.toString());
         }
         else if (action.equals(MeshtasticMapComponent.ACTION_RECEIVED_ATAK_FORWARDER)) {
+            Thread thread = new Thread(() -> receive(intent));
+            thread.setName("MeshtasticReceiver.Worker");
+            thread.start();
+        }
+        else if (action.equals("com.geeksville.mesh.RECEIVED.72")) {
             Thread thread = new Thread(() -> receive(intent));
             thread.setName("MeshtasticReceiver.Worker");
             thread.start();
@@ -68,7 +92,7 @@ public class MeshtasticReceiver extends BroadcastReceiver {
         int dataType = payload.getDataType();
         Log.v(TAG, "handleReceive(), dataType: " + dataType);
 
-        if (dataType == 257) {
+        if (dataType == Portnums.PortNum.ATAK_FORWARDER_VALUE) {
             String message = new String(payload.getBytes());
             if (message.startsWith("CHUNK")) {
                 Log.d(TAG, "Received Chunked message");
@@ -92,7 +116,7 @@ public class MeshtasticReceiver extends BroadcastReceiver {
                 byte[] combined = new byte[cotSize];
 
                 int i = 0;
-                for (byte[] b: chunks) {
+                for (byte[] b : chunks) {
                     try {
                         System.arraycopy(b, 0, combined, i, b.length);
                     } catch (Exception e) {
@@ -100,7 +124,7 @@ public class MeshtasticReceiver extends BroadcastReceiver {
                         Log.d(TAG, "Failed to copy in chunking");
                     }
                     i += b.length;
-                    Log.d(TAG, ""+i);
+                    Log.d(TAG, "" + i);
                 }
 
                 cotSize = 0;
@@ -118,12 +142,11 @@ public class MeshtasticReceiver extends BroadcastReceiver {
                     } else {
                         Log.d(TAG, "Failed to libcotshrink: " + new String(combined));
                     }
-                } catch(Throwable e) {
+                } catch (Throwable e) {
                     e.printStackTrace();
                 }
 
-            }
-            else {
+            } else {
                 try {
 
                     CotEvent cotEvent = MeshtasticMapComponent.cotShrinker.toCotEvent(payload.getBytes());
@@ -136,6 +159,75 @@ public class MeshtasticReceiver extends BroadcastReceiver {
                 }
 
             }
+        } else if (dataType == 72) {
+            Log.d(TAG, "Got TAK_PACKET");
+            try {
+
+                ATAKProtos.TAKPacket tp = ATAKProtos.TAKPacket.parseFrom(payload.getBytes());
+                ATAKProtos.Contact contact = tp.getContact();
+                ATAKProtos.Group group = tp.getGroup();
+                ATAKProtos.Status status = tp.getStatus();
+                ATAKProtos.PLI pli = tp.getPli();
+
+                double lat = pli.getLatitudeI() * 1e-7;
+                double lng = pli.getLongitudeI() * 1e-7;
+                double alt = pli.getAltitude();
+                int course = pli.getCourse();
+                int speed = pli.getSpeed();
+
+                Log.d(TAG, String.format("GPS: %f %f %f", alt, lat ,lng));
+
+                String callsign = contact.getCallsign();
+
+                CotDetail cotDetail = new CotDetail("detail");
+
+                CotDetail contactDetail = new CotDetail("contact");
+                contactDetail.setAttribute("callsign", callsign);
+                //contactDetail.setAttribute("phone", "0");
+                //contactDetail.setAttribute("endpoint", "0.0.0.0:4242:tcp");
+                cotDetail.addChild(contactDetail);
+
+                CotDetail groupDetail = new CotDetail("__group");
+                groupDetail.setAttribute("role", ATAKProtos.MemberRole.forNumber(group.getRoleValue()).name());
+                groupDetail.setAttribute("name", ATAKProtos.Team.forNumber(group.getTeamValue()).name());
+                cotDetail.addChild(groupDetail);
+
+                CotDetail statusDetail = new CotDetail("status");
+                statusDetail.setAttribute("battery", String.valueOf(status.getBattery()));
+                cotDetail.addChild(statusDetail);
+
+                CotDetail trackDetail = new CotDetail("track");
+                trackDetail.setAttribute("speed", String.valueOf(speed));
+                trackDetail.setAttribute("course", String.valueOf(course));
+                cotDetail.addChild(trackDetail);
+
+                CotEvent cotEvent = new CotEvent();
+                cotEvent.setDetail(cotDetail);
+                cotEvent.setUID(callsign);
+
+                CoordinatedTime time = new CoordinatedTime();
+                cotEvent.setTime(time);
+                cotEvent.setStart(time);
+                cotEvent.setStale(time.addMinutes(10));
+
+                cotEvent.setType("a-f-G-U-C");
+
+                cotEvent.setHow("m-e");
+
+                CotPoint cotPoint = new CotPoint(lat, lng, CotPoint.UNKNOWN,
+                        CotPoint.UNKNOWN, CotPoint.UNKNOWN);
+                cotEvent.setPoint(cotPoint);
+
+                if (cotEvent.isValid())
+                    CotMapComponent.getInternalDispatcher().dispatch(cotEvent);
+                else
+                    Log.e(TAG, "cotEvent was not valid");
+
+            } catch (InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
+
+
         }
     }
 }
