4f37009a03fce509690ecdeb5e7f3097b5650a18
Anton Lakida <anton@getbaraka.com>
2025-03-19 19:01:42 +0500

    Use default if the value is not a string

--- app/src/main/java/com/atakmap/android/meshtastic/ProtectedSharedPreferences.kt
+++ app/src/main/java/com/atakmap/android/meshtastic/ProtectedSharedPreferences.kt
@@ -8,11 +8,15 @@ import android.content.SharedPreferences
 class ProtectedSharedPreferences(
     private val preferences: SharedPreferences
 ) : SharedPreferences by preferences {
-    override fun getInt(key: String?, defValue: Int): Int {
-        return try {
-            preferences.getInt(key, defValue);
-        } catch (exception: ClassCastException) {
+    override fun getInt(key: String?, defValue: Int) = try {
+        preferences.getInt(key, defValue)
+    } catch (exception: ClassCastException) {
+        val strValue = preferences.getString(key, defValue.toString())
+        try {
+            strValue?.toInt() ?: defValue
+        } catch (exception: NumberFormatException) {
             defValue
         }
     }
+
 }
